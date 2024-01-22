c8a648405ff0d83ca0366b3821459d4cb95242f6
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-01-22 22:54:18 +0100

    feat: elements

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log('JS OK')
+console.log('JS OK');
+
+// # RECUPERO ELEMENTI
+// img container
+const slidesContainer = document.getElementById('img-container');
+console.log(slidesContainer)
+
+// arrow bot
+const arrowBot = document.getElementById("a-next");
+
+// arrow top 
+const arrowTop = document.getElementById("a-prev");
+
+// valore slide mostrata
+let slideIndex = 0
+
