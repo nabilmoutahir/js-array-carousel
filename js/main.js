df5ce14713ee90687f90ba03078c73c9816b76cf
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-01-22 22:55:59 +0100

    feat: add loop for

--- js/main.js
+++ js/main.js
@@ -18,3 +18,26 @@ let slideIndex = 0
 const imagesCarousel = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp'];
 console.log(imagesCarousel);
 
+// # CICLO FOR
+let slidesHTML = ''
+
+for(let i=0; i < imagesCarousel.length; i++) {
+
+    const image = imagesCarousel[i];
+
+    let activeClass;
+
+    // condizione
+    if(i == slideIndex) {
+        activeClass = 'active'
+    } else {
+        activeClass = '';
+    }
+
+    // elemento da inserire nel container
+    slidesHTML += `<img src="./img/${image}" class="slides ${activeClass}" alt="slides ${i}">`;
+}
+
+slidesContainer.innerHTML = slidesHTML;
+
+
