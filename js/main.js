c6d8eafaa14bb5fab60e3c46b9cf3593288d78f3
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-01-28 20:49:00 +0100

    feat: add interval function

--- js/main.js
+++ js/main.js
@@ -97,3 +97,33 @@ arrowTop.addEventListener ('click', function(){
     const nextSlide = allSlides[slideIndex];
     nextSlide.classList.add('active')
 });
+
+
+function slidesInterval() {
+
+    // recupero slides
+    const allSlides = document.getElementsByClassName('slides');
+
+    // recupero img con sia classe slide che active
+    const currentSlide = document.querySelector('.slides.active');
+
+    // rimuovo classe active
+    const currentSlideSlide = allSlides[slideIndex];
+    currentSlide.classList.remove('active');
+
+    // incremento valore slide mostrata
+    slideIndex;
+
+    // condizione se il volore della slide mostrata è uguale all'ultimo elemento dell'array torna al primo
+    if(slideIndex >= imagesCarousel.length - 1){
+        slideIndex = 0;
+    } else {
+        slideIndex++;
+    }
+
+    // aggiungo la classe
+    const nextSlide = allSlides[slideIndex];
+    nextSlide.classList.add('active')
+
+}
+
